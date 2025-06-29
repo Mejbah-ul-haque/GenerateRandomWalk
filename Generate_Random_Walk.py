1a0fb4a139f2e8dadf865e37c00fd08c50cd0b3c
md-mejbaul-haque <mejba.s@gmail.com>
2025-06-29 13:33:45 +0600

    RGB color function added
    # Conflicts:
    #	Generate_Random_Walk.py

--- Generate_Random_Walk.py
+++ Generate_Random_Walk.py
@@ -3,12 +3,10 @@ import random
 
 tim = t.Turtle()
 
-colors = ["chartreuse", "red", "IndianRed", "blue", "orange", "purple", "pink", "green", "seaGreen"]
 directions =[0, 90, 180, 270]
 tim.pensize(15)
 tim.speed("fast") #fast, fastest, normal, slow, slowest
 
 for _ in range(200):
-    tim.color(random.choice(colors))
     tim.forward(30)
     tim.setheading(random.choice(directions))
